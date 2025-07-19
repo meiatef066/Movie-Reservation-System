fbeb297ac0ce7bbce1f75aa785deeeea63da160a
maiatef066 <maiatef066@gmail.com>
2025-07-20 02:23:51 +0300

    implement admin can promot normal user account to admin ✌

---
+++ movie_reservation_system/src/main/java/com/system/movie_reservation_system/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.system.movie_reservation_system.controller;
+
+import com.system.movie_reservation_system.model.User;
+import com.system.movie_reservation_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/users")
+@PreAuthorize("hasRole('ADMIN')")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/{userId}/promote")
+    public ResponseEntity<Object> promoteUser( @PathVariable Long userId ){
+        var user= userRepository.findById(userId);
+        if(!user.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        user.get().setRole(User.Role.ADMIN);
+        userRepository.save(user.get());
+        return ResponseEntity.ok().build();
+    }
+}
