ba441b8d201919147bdc77da576c6db3e6feda53
maiatef066 <maiatef066@gmail.com>
2025-07-20 20:09:45 +0300

    update v1

--- movie_reservation_system/src/main/java/com/system/movie_reservation_system/controller/AuthController.java
+++ movie_reservation_system/src/main/java/com/system/movie_reservation_system/controller/AuthController.java
@@ -23,50 +23,13 @@ public class AuthController {
     private AuthService authService;
 
     @PostMapping("/login")
-    @Operation(
-            summary = "Login",
-            description = "It allows a user to authenticate itself",
-            requestBody =
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "Payload containing the user credentials.",
-                    required = true,
-                    content = @Content(schema = @Schema(implementation = AuthRequest.class))),
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Authentication successful"),
-                    @ApiResponse(responseCode = "400", description = "Bad credentials"),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Internal Server Error",
-                            content =
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(example = "Internal Server Error")))
-            })
+
     public ResponseEntity<Object> login(@Valid @RequestBody AuthRequest authRequest ) {
         var response = authService.login(authRequest);
         return ResponseEntity.status(response.getStatusCode())
                 .body(response);
     }
 
-    @Operation(
-            summary = "Register",
-            description = "It allows a person to create a user",
-            requestBody =
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(
-                    description = "Payload containing the user credentials.",
-                    required = true,
-                    content = @Content(schema = @Schema(implementation = AuthRequest.class))),
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Registration successful"),
-                    @ApiResponse(responseCode = "400", description = "Email already taken"),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Internal Server Error",
-                            content =
-                            @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(example = "Internal Server Error")))
-            })
     @PostMapping("/register")
     public ResponseEntity<Object> register(@Valid @RequestBody AuthRequest authRequest ) {
         var response = authService.register(authRequest);
